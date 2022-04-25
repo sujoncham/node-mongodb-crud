04668e721dda8b87f07933ecd037459e10dec502
sujon-chambugong <sujonchambugong@gmail.com>
2022-04-25 22:54:48 +0600

    connected with heroku

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+// https://protected-crag-46801.herokuapp.com
+
 //middleware
 app.use(cors());
 app.use(express.json());
